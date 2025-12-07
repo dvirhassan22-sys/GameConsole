06f931a29bd1d1654ed7f18704b9b31b362f3c1c
dvirhassan22-sys <dvir.hassan22@gmail.com>
2025-12-07 18:21:54 +0200

    Implement username and password input in LoginScreen
    Enhanced the LoginScreen class to prompt for username and password during login, and added login validation.

--- GameConsole/Pages/LoginScreen.cs
+++ GameConsole/Pages/LoginScreen.cs
@@ -1,4 +1,5 @@
-﻿using GameConsole.Base;
+using GameConsole.Base;
+using GameConsole.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +8,30 @@ using System.Threading.Tasks;
 
 namespace GameConsole.Pages
 {
-	internal class LoginScreen : Screen
-	{
-		public LoginScreen() : base("Login Page")
-		{
-		}
-		public override void Show()
-		{
-			base.Show();
-			CenterText("Enter Login Info");
-			Console.ReadKey();
-		}
-	}
+    internal class LoginScreen : Screen
+    {
+        public LoginScreen() : base("Login Page")
+        {
+        }
+        public override void Show()
+        {
+            string username, password;
+            base.Show();
+            CenterText("Enter Login Info");
+            Console.Write("Enter your username: ");
+            username = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            password = Console.ReadLine();
+
+            if (UserDb.Login(username, password) != null)
+            {
+                Console.WriteLine("Login succesfull!");
+            }
+            else
+            {
+                Console.WriteLine("Login Failed");
+            }
+        }
+    }
 }
